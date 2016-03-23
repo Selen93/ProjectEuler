3e6474e064a99562d9c970a7f18fc729ab30166c
Nathalie Bargenda <n.bargenda@gmail.com>
2016-03-23 13:48:57 +0100

    Problem 2

---
+++ Python/Problem 2/Problem 2.py
@@ -0,0 +1,25 @@
+# Sum of even Fibonacci Numbers until 4 Million
+
+
+def F(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return F(n-1)+F(n-2)
+
+
+result = 0
+fibonaccinumbers = list()
+x=0
+
+while True:
+    x = x+1
+    if F(x) >= 4000000:
+        break
+    fibonaccinumbers.append(F(x))
+
+for x in fibonaccinumbers:
+    if x%2==0:
+        result = result +x
+
+print(result)
+
