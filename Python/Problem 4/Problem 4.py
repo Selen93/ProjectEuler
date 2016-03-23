c3daa105905fe69db687e86226dcbede0e0829ba
Nathalie Bargenda <n.bargenda@gmail.com>
2016-03-23 23:18:28 +0100

    Problem 4

---
+++ Python/Problem 4/Problem 4.py
@@ -0,0 +1,14 @@
+# Largest palindrome number from the product of two 3-digit numbers
+
+zahl = ""
+palindromzahlen = []
+
+for x in range (100,999):
+    for y in range (100,999):
+        zahl = str(x*y)
+        if len(zahl)==6: #With 5 digit palindrome numbers, result is 99999 ?!
+             if zahl == zahl[::-1]:
+                 palindromzahlen.append(zahl)
+
+
+print(max(palindromzahlen))
