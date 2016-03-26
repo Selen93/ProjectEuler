7995d3e5b6ce31d3d093bc1bd91e9113adfdb8b5
Nathalie Bargenda <n.bargenda@gmail.com>
2016-03-26 20:30:24 +0100

    finished Problem 15

--- Python/Problem 15/Problem 15.py
+++ Python/Problem 15/Problem 15.py
@@ -1,13 +1,15 @@
 # how many routes are in a 20x20 lattice grid?
-# incomplete
 
-row = []
 field = []
-count = 0
-for i in range(20):
-    row.append(0)
-for i in range(20):
-    field.append(row)
-field[0][0] = 1
+sum = 0
 
+for i in range(21):
+    field.append([1])
 
+for n in range(0,21):
+    for i in range(1,21):
+        sum = field[n][i-1]
+        if n>0: sum += field[n-1][i]
+        field[n].append(sum)
+
+print(field)
