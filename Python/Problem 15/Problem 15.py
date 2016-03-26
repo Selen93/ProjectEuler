fab9206734ec87fa0753df94e59ca8713973157d
Nathalie Bargenda <n.bargenda@gmail.com>
2016-03-26 20:47:19 +0100

    finished Problem 15

--- Python/Problem 15/Problem 15.py
+++ Python/Problem 15/Problem 15.py
@@ -1,15 +1,16 @@
 # how many routes are in a 20x20 lattice grid?
+import time
 
+start_time = time.clock()
 field = []
 sum = 0
 
-for i in range(21):
-    field.append([1])
-
 for n in range(0,21):
+    field.append([1])
     for i in range(1,21):
         sum = field[n][i-1]
         if n>0: sum += field[n-1][i]
         field[n].append(sum)
 
-print(field)
+print(field[20][20])
+print("--- %s seconds ---" % (time.clock() - start_time))
