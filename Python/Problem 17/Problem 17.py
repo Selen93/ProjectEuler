42e5cf077a538006f535ff5afb2cd43ce1b1e579
Nathalie Bargenda <n.bargenda@gmail.com>
2016-03-27 12:34:47 +0200

    Problem 17 WORKS

--- Python/Problem 17/Problem 17.py
+++ Python/Problem 17/Problem 17.py
@@ -1,9 +1,8 @@
 # Number letter counts from 1 to 1000
-# Error -10 ?!
 import time
 
 start_time = time.clock()
-numbers = {1:3,2:3,3:5,4:4,5:4,6:3,7:5,8:5,9:4,10:3,11:6,12:6,13:8,14:8,15:7,16:7,17:8,18:8,19:8,20:6,30:6,40:5,50:5,60:5,70:7,80:6,90:6,100:10,1000:11}
+numbers = {1:3,2:3,3:5,4:4,5:4,6:3,7:5,8:5,9:4,10:3,11:6,12:6,13:8,14:8,15:7,16:7,17:9,18:8,19:8,20:6,30:6,40:5,50:5,60:5,70:7,80:6,90:6,100:10,1000:11}
 count = 0
 
 #numbers from 21 to 99
@@ -22,7 +21,6 @@ for i in range(100,1000,100):
 
 for key in numbers:
     count += numbers.get(key)
-print(str(numbers))
-print(count)
 
+print(count)
 print("--- %s seconds ---" % (time.clock() - start_time))
